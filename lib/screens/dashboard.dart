431e6387c345e5994e3942ead1d374b8497c8ed3
Kakomo <kakomo2@gmail.com>
2020-09-17 21:28:48 -0300

    Class 1 - 1 Lesson

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -16,45 +16,58 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => ContactsList(),
-                  ));
-                },
-                child: Container(
-                  padding: EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(
-                        Icons.people,
-                        size: 24.0,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        'Contacts',
-                        style: TextStyle(fontSize: 16.0, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+          Row(
+            children: [
+              _FeatureItem('Transfer',Icons.monetization_on),
+              _FeatureItem('Transaction Feed',Icons.description)
+            ],
           ),
+
         ],
       ),
     );
   }
 }
 
-
-
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  _FeatureItem(this.name, this.icon);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => ContactsList(),
+            ));
+          },
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(
+                  icon,
+                  size: 24.0,
+                  color: Colors.white,
+                ),
+                Text(
+                  name,
+                  style: TextStyle(fontSize: 16.0, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
