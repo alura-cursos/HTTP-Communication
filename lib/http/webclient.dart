2d89121ff04dd780e3fa67e59485eac671af7adc
Kakomo <kakomo2@gmail.com>
2020-09-24 00:42:34 -0300

    Class 2 - 3 Lesson

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,7 +1,29 @@
 
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('Request \n');
+    print('URL: ${data.url}');
+    print('Headers: ${data.headers}');
+    print('Body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('Response \n');
+    print('Status Code: ${data.statusCode}');
+    print('Headers: ${data.headers}');
+    print('Body: ${data.body}');
+    return data;
+  }
+
+}
 
 void findAll() async {
-  final Response response = await get('http://192.168.0.6:8080/transactions');
-  print(response.body);
+  Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+  final Response response = await client.get('http://192.168.0.6:8080/transactions');
 }
